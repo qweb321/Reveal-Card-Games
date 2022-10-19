d406f4e3b33466b86d87faf293db8dba59f030bd
Isis Lin <qweb32114@gmail.com>
2022-10-19 14:20:55 +0800

    Edit view for game finished and add comment

--- main.js
+++ main.js
@@ -61,30 +61,38 @@ const view = {
   },
 
   renderScore(score) {
-    document.querySelector('.score').textContent = `Score: ${score}`
+    document.querySelector(".score").textContent = `Score: ${score}`;
   },
 
   renderTimes(times) {
-    document.querySelector('.times').textContent = `You've tried ${times} times` 
+    document.querySelector(
+      ".times"
+    ).textContent = `You've tried ${times} times`;
   },
 
-  wrongAnimation(...cards){
+  wrongAnimation(...cards) {
     cards.map((card) => {
-        card.classList.add("wrong")
-        card.addEventListener('animationend', event => {
-            event.target.classList.remove('wrong')
-        }, {once:true})
-    })
+      card.classList.add("wrong");
+
+      // When animation end, it will remove wrong class
+      card.addEventListener(
+        "animationend",
+        (event) => {
+          event.target.classList.remove("wrong");
+        },
+        { once: true }
+      ); //once: If true, the listener will be remove after invoked
+    });
   },
 
-  gameFinished(){
-    const div = document.createElement('div')
-    div.classList.add('finished')
-    div.innerHTML = `<
+  gameFinished() {
+    const div = document.createElement("div");
+    div.classList.add("finished");
+    div.innerHTML = `
     <h1>Completed!</h1>
     <p>Score is ${modal.score}</p>
-    <p>Yoy've tried ${modal.times} times</p>`
-    document.body.append(div)
+    <p>Yoy've tried ${modal.times} times</p>`;
+    document.body.appendChild(div);
   },
 
   displayCards(indexes) {
@@ -92,8 +100,8 @@ const view = {
     rootElement.innerHTML = indexes
       .map((index) => this.getCardElement(index))
       .join(" ");
-    
-      // in forEach way
+
+    // in forEach way
     // indexes.forEach(
     //   (index) => (rootElement.innerHTML += this.getCardElement(index))
     // );
@@ -103,6 +111,7 @@ const view = {
 const uility = {
   getRandomCard(count) {
     const number = Array.from(Array(count).keys());
+    // Fisher-Yates Shuffle
     for (let index = number.length - 1; index > 0; index--) {
       let randomIndex = Math.floor(Math.random() * (index + 1));
       [number[index], number[randomIndex]] = [
@@ -128,7 +137,7 @@ const modal = {
   times: 0,
 };
 
-const controll = {
+const controller = {
   currentState: Game_State.FistCardAwaite, // Game initialize
 
   generateCard() {
@@ -147,43 +156,45 @@ const controll = {
         this.currentState = Game_State.SecondCardAwaite;
         break;
       case Game_State.SecondCardAwaite:
-        view.renderTimes(++modal.times)
+        view.renderTimes(++modal.times);
         view.filpCards(card);
         modal.revealedCards.push(card);
         if (modal.isrevealedCardsMatched()) {
-          //pair sucessfully
+          //pair successfully
           this.currentState = Game_State.CardsMatch;
-          view.renderScore(modal.score += 10)
+          view.renderScore((modal.score += 10));
           view.pairedCards(...modal.revealedCards);
           modal.revealedCards = [];
-          if(modal.score === 260) {
-            this.currentState = Game_State.GameFinished
-            view.gameFinished()
-            return
+          if (modal.score === 260) {
+            this.currentState = Game_State.GameFinished;
+            view.gameFinished();
+            return;
           }
           this.currentState = Game_State.FistCardAwaite;
         } else {
           //pair failed
           this.currentState = Game_State.CardsMatchFailed;
-          view.wrongAnimation(...modal.revealedCards)
-          setTimeout(() => {
-            view.filpCards(...modal.revealedCards);
-            modal.revealedCards = [];
-            controll.currentState = Game_State.FistCardAwaite;
-          }, 1000);
+          view.wrongAnimation(...modal.revealedCards);
+          setTimeout(controller.resetCard, 1000);
         }
         break;
     }
   },
+
+  resetCard() {
+    view.filpCards(...modal.revealedCards);
+    modal.revealedCards = [];
+    controller.currentState = Game_State.FistCardAwaite;
+  },
 };
 
-controll.generateCard();
+controller.generateCard();
 
 document
   .querySelector("#cards")
   .addEventListener("click", function onCardClik(event) {
     const card = event.target;
     if (card.classList.contains("card")) {
-      controll.dispatchCardAction(card);
+      controller.dispatchCardAction(card);
     }
   });
